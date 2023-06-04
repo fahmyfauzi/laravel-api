83ca181995f285ab9c1d51af842edb98ae288915
fahmyfauzi <fahmyfauzii@gmail.com>
2023-06-04 11:30:51 +0700

    FE index

---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Buku</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body class="bg-light">
+    <main class="container">
+        <!-- START FORM -->
+        <div class="bg-body my-3 rounded p-3 shadow-sm">
+            <form action='' method='post'>
+                <div class="row mb-3">
+                    <label for="judul" class="col-sm-2 col-form-label">Judul Buku</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name='judul' id="judul">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="nama" class="col-sm-2 col-form-label">Pengarang</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name='pengarang' id="pengarang">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="tanggal_publikasi" class="col-sm-2 col-form-label">Tanggal Publikasi</label>
+                    <div class="col-sm-10">
+                        <input type="date" class="form-control w-50" name='tanggal_publikasi' id="tanggal_publikasi">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- AKHIR FORM -->
+
+        <!-- START DATA -->
+        <div class="bg-body my-3 rounded p-3 shadow-sm">
+            <table class="table-striped table">
+                <thead>
+
+
+
+                    <tr>
+                        <th class="col-md-1">No</th>
+                        <th class="col-md-4">Judul</th>
+                        <th class="col-md-3">Pengarang</th>
+                        <th class="col-md-2">Tanggal Publikasi</th>
+                        <th class="col-md-2">Aksi</th>
+                    </tr>
+
+
+                </thead>
+                <tbody>
+                    @php
+                        $no = 1;
+                    @endphp
+                    @foreach ($data as $item)
+                        <tr>
+                            <td>{{ $no }}</td>
+                            <td>{{ $item['judul'] }}</td>
+                            <td>{{ $item['pengarang'] }}</td>
+                            <td>{{ date('d-m-Y', strtotime($item['tanggal_publikasi'])) }}</td>
+                            <td>
+                                <a href="" class="btn btn-warning btn-sm">Edit</a>
+                                <a href="" class="btn btn-danger btn-sm">Del</a>
+                            </td>
+                        </tr>
+                        @php
+                            $no++;
+                        @endphp
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+        <!-- AKHIR DATA -->
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
